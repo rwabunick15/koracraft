98d971585a0b58ddba7e40312d535c4c67295792
rwabunick15 <nickrwabuneza@gmail.com>
2026-07-17 22:47:41 +0200

    Allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ app.use(helmet());
 app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://koracraft.vercel.app/"
     ],
     credentials: true
 }));
